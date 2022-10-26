2505224c6c0a8ce194edbb5a0abfcfa0d1161713
HYEON\Lenovo <chohmk@naver.com>
2022-10-27 00:02:11 +0900

    no message

--- src/main/java/com/sns/comment/CommentRestController.java
+++ src/main/java/com/sns/comment/CommentRestController.java
@@ -5,14 +5,19 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sns.comment.bo.CommentBO;
+
 @RestController
 @RequestMapping("/comment")
 public class CommentRestController {
+	@Autowired
+	private CommentBO commentBO;
 	@PostMapping("/create")
 	public Map<String, Object> create(
 			@RequestParam("postId") int postId, 
@@ -34,6 +39,7 @@ public class CommentRestController {
 		}
 		
 		// bo
+		commentBO.addComment(postId, postId, content);
 		return result;
 	}
 	
--- src/main/java/com/sns/comment/bo/CommentBO.java
+++ src/main/java/com/sns/comment/bo/CommentBO.java
@@ -1,10 +1,15 @@
 package com.sns.comment.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sns.comment.dao.CommentDAO;
 
 @Service
 public class CommentBO {
+	@Autowired
+	private CommentDAO commentDAO;
 	public int addComment(int userId, int postId, String content) {
-		return ;
+		return commentDAO.insertComment(userId, postId, content);
 	}
 }
